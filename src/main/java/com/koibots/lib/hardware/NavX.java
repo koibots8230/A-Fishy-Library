1cc1e4b5b3e2582640cd6e649f8bf4a560085252
CalebNeal07 <caleboneal07@gmail.com>
2023-07-23 14:32:06 -0400

    Cleaned up NavX class

--- src/main/java/com/koibots/lib/hardware/NavX.java
+++ src/main/java/com/koibots/lib/hardware/NavX.java
@@ -1,26 +1,18 @@
 package com.koibots.lib.hardware;
 
-
 import com.kauailabs.navx.frc.AHRS;
-
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
-
+/** Singleton wrapper for AHRS class.
+ * @author Caleb O'Neal
+ */
 public class NavX extends AHRS{
   private static final NavX gyro = new NavX();
 
-
   NavX() {
     super(SPI.Port.kMXP);
-
-
-    if (DriverStation.getAlliance() == Alliance.Red) {
-      this.setAngleAdjustment(180);
-    }
   }
-
 
   public static NavX get() {
     return gyro;
