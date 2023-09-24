be74a8ce31e6067aefe5fa04ac499dc44f3878db
CalebNeal07 <caleboneal07@gmail.com>
2023-09-23 23:18:29 -0400

    Incorporated AdvantageKit

--- src/main/java/com/koibots/lib/hardware/NavX.java
+++ src/main/java/com/koibots/lib/hardware/NavX.java
@@ -2,7 +2,6 @@ package com.koibots.lib.hardware;
 
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 /** Singleton wrapper for AHRS class.
  * @author Caleb O'Neal
